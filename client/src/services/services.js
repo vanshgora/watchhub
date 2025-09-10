fd5cba0b60c6770759cb7e619849eb2ca5966400
vanshgora <vanshgora31@gmail.com>
2025-09-10 13:07:15 +0530

    added cloudfalre tunnel

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = 'http://localhost:8000'
+const API_URL = 'https://occupied-they-shared-successfully.trycloudflare.com'
 
 const signup = async (data) => {
     try {
